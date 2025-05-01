34e077d7892813f47e7535cbb3296fcdfe6d3a1a
amcdonald <e_dorion@hotmail.com>
2025-05-01 13:04:44 +1000

    add AWX

--- provision.sh
+++ provision.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# to pick over for improvments https://github.com/arashkaffamanesh/kubeadm-multipass i.e. https://mkcert.org/
-
 date
 mkdir masterkube >/dev/null 2>&1
 mkdir workerkube >/dev/null 2>&1
@@ -16,6 +14,11 @@ multipass delete --all -p
 multipass launch -m 8Gb -d 150G -c 4 -n pmaster --mount ./masterkube:/home/ubuntu/.kube
 #multipass launch -m 8Gb -d 150G -c 4 -n pworker1 --mount ./workerkube:/home/ubuntu/.kube
 
+# Install Caddy
+multipass exec pmaster -- sudo apt install caddy -y
+multipass exec pmaster -- sudo cp /home/ubuntu/.kube/Caddyfile /etc/caddy/Caddyfile
+multipass exec pmaster -- sudo systemctl restart caddy
+
 # Install Microk8's
 multipass exec pmaster -- sudo snap install microk8s --classic
 multipass exec pmaster -- sudo snap install helm --classic
@@ -24,11 +27,6 @@ multipass exec pmaster -- sudo snap install kubeadm --classic
 #multipass exec pworker1 -- sudo snap install microk8s --classic
 #multipass exec pworker1 -- sudo snap install helm --classic
 #multipass exec pworker1 -- sudo snap alias microk8s.kubectl kubectl
-
-# Install Caddy
-multipass exec pmaster -- sudo snap install --edge caddy
-multipass exec pmaster -- sudo cp /home/ubuntu/.kube/Caddyfile /etc/caddy/Caddyfile
-multipass exec pmaster -- sudo systemctl restart caddy
 
 # Make the user a sudoer, disable firewall within the vm and enable k8 dashboard and storage.
 multipass exec pmaster -- sudo usermod -a -G microk8s ubuntu
@@ -58,15 +56,8 @@ multipass exec pmaster -- sudo microk8s enable hostpath-storage
 echo "###### Installing Headlamp & Cribl ######"
 multipass exec pmaster -- helm repo add headlamp https://kubernetes-sigs.github.io/headlamp/
 multipass exec pmaster -- helm install headlamp headlamp/headlamp -f /home/ubuntu/.kube/headlampValues.yaml --namespace kube-system
-#multipass exec pmaster -- kubectl apply -f /home/ubuntu/.kube/headlamp.yaml
-
-multipass exec pmaster --helm repo add cribl https://criblio.github.io/helm-charts/
-multipass exec pmaster --helm install -f /home/ubuntu/.kube/CriblValues.yaml
-
-#helm repo add elastic https://helm.elastic.co
-#helm repo update
-# Install an eck-managed Elasticsearch, Kibana, Beats and Logstash using custom values.
-#helm install eck-stack-with-logstash elastic/eck-stack --values https://raw.githubusercontent.com/elastic/cloud-on-k8s/2.16/deploy/eck-stack/examples/logstash/basic-eck.yaml -n elastic-stack
+multipass exec pmaster -- helm repo add cribl https://criblio.github.io/helm-charts/
+multipass exec pmaster -- helm install -f /home/ubuntu/.kube/CriblValues.yaml
 
 # install Vault
 #echo "###### Installing Vault ######"
@@ -79,35 +70,16 @@ multipass exec pmaster --helm install -f /home/ubuntu/.kube/CriblValues.yaml
 #VAULT_UNSEAL_KEY=$(cat cluster-keys.json | jq -r ".unseal_keys_b64[]")
 #multipass exec pmaster -- kubectl -n $VAULT_K8S_NAMESPACE exec vault-0 -- vault operator unseal $VAULT_UNSEAL_KEY
 
-# install Pi-hole
-echo "###### Installing Pi-hole ######"
-multipass exec pmaster -- helm repo add savepointsam https://savepointsam.github.io/charts
-multipass exec pmaster -- helm repo update
-multipass exec pmaster -- helm install my-release savepointsam/pihole
-
-# install HomeAssistant
-echo "###### Installing HomeAssistant ######"
-multipass exec pmaster -- helm repo add pajikos http://pajikos.github.io/home-assistant-helm-chart/
-multipass exec pmaster -- helm repo update
-multipass exec pmaster -- helm install home-assistant pajikos/home-assistant -f /home/ubuntu/.kube/homeassistantValues.yaml
-# install IT Tools
-echo "###### Installing IT Tools ######"
-multipass exec pmaster -- helm repo add jeffresc https://charts.jeffresc.dev
-multipass exec pmaster -- helm repo update
-multipass exec pmaster -- helm install it-tools jeffresc/it-tools
+# Install AWX
+echo "###### Installing AWX ######"
+multipass exec pmaster -- helm repo add awx-operator https://ansible-community.github.io/awx-operator-helm/
+multipass exec pmaster -- helm install my-awx-operator awx-operator/awx-operator -n awx --create-namespace -f /home/ubuntu/.kube/AWX.yaml
 
 # install Homepage
 echo "###### Installing Homepage ######"
 multipass exec pmaster -- helm repo add jameswynn https://jameswynn.github.io/helm-charts
 multipass exec pmaster -- helm repo update
 multipass exec pmaster -- helm install homepage jameswynn/homepage -f /home/ubuntu/.kube/homepageValues.yaml
-
-#install postgress
-#multipass exec pmaster -- helm install my-release oci://registry-1.docker.io/bitnamicharts/postgresql
-
-#install BoundaryController
-#multipass exec pmaster -- kubectl apply -f ./.kube/boundaryControllerConfigMap.yaml
-#multipass exec pmaster -- kubectl apply -f ./.kube/boundaryController.yaml
 
 # install test nginx app
 #multipass exec pmaster -- kubectl apply -f ./.kube/mysite.yaml
